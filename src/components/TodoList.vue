f34ef49785d771d94a17af46339a04c0b736f868
Alexander Krueger <alexander.krueger@centogene.com>
2019-07-09 09:43:14 +0200

    Added CSV - Download for filtered Todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,18 +3,23 @@
     <v-toolbar light>
       <v-toolbar-title>Todo List</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-text-field :label="``" box autocomplete="on" type="text" v-model="searchString" placeholder="Filter..."></v-text-field>
+      <v-text-field :label="``" box autocomplete="on" type="text" @change="updateCSVDownload('#csvDataSet', getCSVTodos(true), 'Filtered_CSV_Download_'+ searchString +'_'+ new Date().toISOString()+'.csv')" v-model="searchString" placeholder="Filter..."></v-text-field>
     </v-toolbar>
     <TodoListItem v-for="todo in filteredTodosBySearchString" v-bind:todo="todo" v-bind:key="todo.id"></TodoListItem>
+    <v-btn><a id="csvDataSet" download="" href="#">Export CSV</a></v-btn>
   </div>
 </template>
  
 <script>
   import TodoListItem from './TodoListItem';
+
+  //only needed to create CSV-File on Browser-Side, should be removed when csv-Generation comes from Server side
+  import JQuery from 'jquery'
+  let $ = JQuery   
  
   export default {
     async mounted() {
-      
+      this.updateCSVDownload('#csvDataSet', this.getCSVTodos(true), 'Filtered_CSV_Download_'+ this.searchString +'_'+ new Date().toISOString()+'.csv')
     },
     components: {
       TodoListItem
@@ -22,17 +27,48 @@
     name: 'TodoList',
     data() {
       return {
-        searchString: ''
+        searchString: '',
+        blobObject: null
       };
     },
     computed: {
         filteredTodosBySearchString() {
-          return this.$store.getters.todos.filter(todoItem => 
+          return this.filterTodosBySearchString();
+        }
+    },
+    methods:{
+        getCSVTodos(filtered) {
+          var filteredTodos = filtered ? this.filterTodosBySearchString() : this.$store.getters.todos;
+          var csv = ""; 
+          if (filteredTodos.length > 0){
+            const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+            const header = Object.keys(filteredTodos[0]);
+            csv = filteredTodos.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+            csv.unshift(header.join(','));
+            csv = csv.join('\r\n');
+          }
+          return csv;
+        },
+        filterTodosBySearchString() {
+          return this.searchString.length > 0 ? (this.$store.getters.todos.filter(todoItem => 
             (todoItem.title.toUpperCase().indexOf(this.searchString.toUpperCase()) !== -1) ||
             (todoItem.referenceLink.toUpperCase().indexOf(this.searchString.toUpperCase()) !== -1) ||
             (this.$store.getters.statusTypes.find(statusType => statusType.id === todoItem.statusTypeId).displayName.toUpperCase().indexOf(this.searchString.toUpperCase()) !== -1) ||
             (todoItem.description.toUpperCase().indexOf(this.searchString.toUpperCase()) !== -1)
-          );
+          )) : this.$store.getters.todos;
+        },
+        updateCSVDownload(anchorSelector, content, fileName){
+          if(window.navigator.msSaveOrOpenBlob) {
+            var fileData = [content];
+            this.blobObject = new Blob(fileData);
+            $(anchorSelector).click(function(){
+              window.navigator.msSaveOrOpenBlob(this.blobObject, fileName);
+            });
+          } else {
+            var url = "data:text/plain;charset=utf-8," + encodeURIComponent(content);
+            $(anchorSelector).attr("href", url);
+            $(anchorSelector).attr("download", fileName);
+          }
         }
     }
   }
